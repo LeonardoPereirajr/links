bb041fceb44553786192e31d3415de63ef65d161
LEONARDO PEREIRA <30580018+LeonardoPereirajr@users.noreply.github.com>
2023-02-07 22:17:33 -0300

    Criação de esboço método POST em pagina HTML

--- src/main/java/com/leonardo/devnology/links/LinksApplication.java
+++ src/main/java/com/leonardo/devnology/links/LinksApplication.java
@@ -3,6 +3,11 @@ package com.leonardo.devnology.links;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @SpringBootApplication
 public class LinksApplication {
@@ -15,6 +20,22 @@ public class LinksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LinksApplication.class, args);
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+		return templateEngine;
+	}
+
+	@Bean
+	public ITemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setPrefix("classpath:/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		return templateResolver;
 	}
 
 }
